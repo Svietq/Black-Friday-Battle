197ee99e37797659c0d92955babd43030445c776
Michał Świętek <mswietku@gmail.com>
2019-04-18 21:34:10 +0200

    Implement better player movement

--- godot/characters/player.gd
+++ godot/characters/player.gd
@@ -2,11 +2,19 @@ extends "res://characters/character.gd"
 
 func control(delta):
 	velocity = Vector2()
+	
 	if Input.is_action_pressed('ui_up'):
-		velocity += Vector2(0, -speed)
+		velocity += Vector2(0, -1)
+			
 	if Input.is_action_pressed('ui_down'):
-		velocity += Vector2(0, speed)
+		velocity += Vector2(0, 1)
+			
 	if Input.is_action_pressed('ui_left'):
-		velocity += Vector2(-speed, 0)
+		velocity += Vector2(-1, 0)
+			
 	if Input.is_action_pressed('ui_right'):
-		velocity += Vector2(speed, 0)
+		velocity += Vector2(1, 0)
+		
+	if velocity != Vector2():
+		set_rotation(velocity.angle())
+		velocity = velocity.normalized() * speed
