caf477aec6c54fede6b7a280e087663c8dc84551
Michał Świętek <mswietku@gmail.com>
2019-04-18 22:44:28 +0200

    Rotate player's directions of movement to match angles on the floor

--- godot/characters/player.gd
+++ godot/characters/player.gd
@@ -1,19 +1,22 @@
 extends "res://characters/character.gd"
 
+const Y_AXIS_ROTATION = deg2rad(63.5)
+const X_AXIS_ROTATION = deg2rad(26.5)
+
 func control(delta):
 	velocity = Vector2()
 	
 	if Input.is_action_pressed('ui_up'):
-		velocity += Vector2(0, -1)
+		velocity += Vector2(0, -1).rotated(Y_AXIS_ROTATION)
 			
 	if Input.is_action_pressed('ui_down'):
-		velocity += Vector2(0, 1)
+		velocity += Vector2(0, 1).rotated(Y_AXIS_ROTATION)
 			
 	if Input.is_action_pressed('ui_left'):
-		velocity += Vector2(-1, 0)
+		velocity += Vector2(-1, 0).rotated(X_AXIS_ROTATION)
 			
 	if Input.is_action_pressed('ui_right'):
-		velocity += Vector2(1, 0)
+		velocity += Vector2(1, 0).rotated(X_AXIS_ROTATION)
 		
 	if velocity != Vector2():
 		set_rotation(velocity.angle())
