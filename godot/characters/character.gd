8028d3a79f7a6dbd5881c1a9331a17784c5739f7
Ssect0r <wojasinskipi@gmail.com>
2019-05-05 22:36:46 +0200

    Koszyk
    Dodanie koszyka (1 element do poprawy)

--- godot/characters/character.gd
+++ godot/characters/character.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-
+var actionPressed:bool = false
 export (int) var speed
 
 var velocity = Vector2()
@@ -8,6 +8,7 @@ func control(delta):
 	pass
 
 func _physics_process(delta):
+	actionPressed = false
 	control(delta)
 	move_and_slide(velocity)
 
