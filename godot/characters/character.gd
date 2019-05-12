1b81a7a93608cd80587a563b7ef63b94754da77e
Ssect0r <wojasinskipi@gmail.com>
2019-05-12 12:30:36 +0200

    Poprawka do koszyka

--- godot/characters/character.gd
+++ godot/characters/character.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
-var actionPressed:bool = false
-export (int) var speed
 
+
+export (int) var speed
 var velocity = Vector2()
+var actionPressed:bool = false
+onready var Trolley_handle = $"../Trolley"
 
 func control(delta):
 	pass
 
 func _physics_process(delta):
-	actionPressed = false
+	Trolley_handle.isInUse = false
 	control(delta)
 	move_and_slide(velocity)
-
-
+#	if Trolley_handle.position.distance_to(position)<100 && actionPressed && Trolley_handle.isInUse == false:
+##		Trolley_handle.position += velocity.normalized()
+#		Trolley_handle.position.linear_interpolate()
+#		Trolley_handle.rotation = transform.get_rotation()	
+#		Trolley_handle.isInUse = true
+#
+#
--- godot/characters/player.gd
+++ godot/characters/player.gd
@@ -1,10 +1,11 @@
 extends "res://characters/character.gd"
-onready var Trolley_handle = $"../Trolley"
+
+
 
 func control(delta):
 	velocity = Vector2()
-
-	Trolley_handle.isInUse = false
+	
+	actionPressed = false
 	
 	if Input.is_action_pressed('ui_up'):
 		velocity += Vector2(0, -1)
@@ -18,20 +19,19 @@ func control(delta):
 	if Input.is_action_pressed('ui_right'):
 		velocity += Vector2(1, 0)
 		
-	if Input.is_action_pressed("ui_select"):
+	if Input.is_action_pressed('ui_select'):
 		actionPressed = true
-		
-	if Trolley_handle.position.distance_to(position)<200 && actionPressed && Trolley_handle.isInUse == false:
-		#Trolley_handle.position += velocity.normalized()
-		Trolley_handle.position = Trolley_handle.position.linear_interpolate(position + velocity.normalized()*50, 0.1)
-		Trolley_handle.rotation = transform.get_rotation()
-		Trolley_handle.isInUse = true
+
+			
 
 	if velocity != Vector2():
 		set_rotation(velocity.angle())
-		#velocity = cartesian_to_isometric(velocity)
 		velocity = velocity.normalized() * speed
-		
 
-func cartesian_to_isometric(cartesian):
-	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / 2)
+		if Trolley_handle.position.distance_to(position)<200 && actionPressed && Trolley_handle.isInUse == false:
+	#		Trolley_handle.position += velocity.normalized()
+			Trolley_handle.position = Trolley_handle.position.linear_interpolate(position + velocity.normalized()*100, 1.0)
+			Trolley_handle.rotation = transform.get_rotation()	
+			Trolley_handle.isInUse = true
+
+
