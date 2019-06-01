956ec6dda85b6627af231fbbc54a09f3a8174861
Ssect0r <wojasinskipi@gmail.com>
2019-06-01 12:44:17 +0200

    Basic timer elo

--- godot/pickable objects/pickableObjects.gd
+++ godot/pickable objects/pickableObjects.gd
@@ -3,11 +3,17 @@ extends RigidBody2D
 var picked = false
 
 func _physics_process(delta):
+	var name   = $CollisionShape2D.get_parent().get_name()
+	var bodies = $detector.get_overlapping_bodies()
+	if name == "Trolley":
+		print(self.get_colliding_bodies())
+	for b in bodies:
+		print(b.name)
+		print(b.get_class())
 	if picked == true:
 		self.position = get_node("../player/Position2D").get_transform().get_origin()
-		sleeping = true
-	else:
-		sleeping = false
+		self.rotation = get_node("../player/Position2D").get_transform().get_rotation()
+
 	
 
 func _input(event):
@@ -19,4 +25,8 @@ func _input(event):
 				get_node("../player").can_pick = false
 	if Input.is_action_just_released("ui_accept"):
 		picked = false
+		var bodies = $detector.get_overlapping_bodies()
+		for b in bodies:
+			if b.name == "Trolley":
+				print("polozone")
 		get_node("../player").can_pick = true
