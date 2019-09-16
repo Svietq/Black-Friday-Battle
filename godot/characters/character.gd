27eeb8e78244db796afcd9e0b4294ec4d2b527f2
Michał Świętek <mswietku@gmail.com>
2019-09-17 00:09:08 +0200

    Implement countdown timer

--- godot/characters/character.gd
+++ godot/characters/character.gd
@@ -15,5 +15,5 @@ func _physics_process(delta):
 	if collisionData and collisionData.collider is preload("res://pickable objects/pickableObjects.gd"):
 		collisionData.collider.queue_free()
 		items_counter += 1
-		get_node("GUI/Label").set_text(str(items_counter))
+		get_parent().get_node("CanvasLayer/GUI/HBoxContainer/ItemsCounter").set_text(" Items collected: " + str(items_counter))
 	
