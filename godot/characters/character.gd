174ab66b7f74109a7015119b8918809cd7dfd590
Michał Świętek <mswietku@gmail.com>
2019-09-16 23:13:09 +0200

    Implement items counter

--- godot/characters/character.gd
+++ godot/characters/character.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 export (int) var speed
 var velocity = Vector2()
 var can_pick = true
+var items_counter = 0
 
 func control(delta):
 	pass
@@ -13,4 +14,6 @@ func _physics_process(delta):
 	var collisionData = move_and_collide(velocity * delta)
 	if collisionData and collisionData.collider is preload("res://pickable objects/pickableObjects.gd"):
 		collisionData.collider.queue_free()
+		items_counter += 1
+		get_node("GUI/Label").set_text(str(items_counter))
 	
